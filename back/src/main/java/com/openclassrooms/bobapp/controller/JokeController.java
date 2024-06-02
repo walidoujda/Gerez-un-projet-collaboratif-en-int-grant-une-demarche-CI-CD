00c42de6324f008512cc5afdd5863da59a1c57e0
Walid ZERRIFI <walidzerrifi@gmail.com>
2024-06-02 21:13:17 +0200

    003 test commit

--- back/src/main/java/com/openclassrooms/bobapp/controller/JokeController.java
+++ back/src/main/java/com/openclassrooms/bobapp/controller/JokeController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.openclassrooms.bobapp.service.JokeService;
 
-@RestController 
+@RestController
 @RequestMapping("api/joke")
 public class JokeController {
 
